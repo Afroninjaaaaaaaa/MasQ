0248922e97a63da838c739f7caaffa9fad95d77e
AfroNinjaaaaaaaa <m1105.108.megaman@gmail.com>
2017-05-16 18:37:24 +0200

    Added 3D model feelings simulation (script to be fixed)

---
+++ LegoFeelings/Assets/Scripts/ChangeFace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeFace : MonoBehaviour
+{
+    Material DefaultFaceMat;
+    Texture DefaultFaceTex;
+    Shader DefaultShader;
+
+    void Start ()
+    {
+        DefaultFaceMat = Resources.Load("Materials/FaceAngry.mat", typeof(Material)) as Material;
+        DefaultFaceTex = Resources.Load("Textures/angry.png", typeof(Texture)) as Texture;
+        MeshRenderer faceRenderer  = gameObject.AddComponent<MeshRenderer>() as MeshRenderer;
+        DefaultShader = Shader.Find("Sprites/Default");
+
+        //faceRenderer.material.mainTexture = DefaultFaceTex;
+        GetComponent<Renderer>().material.SetTexture("_MainTex", DefaultFaceTex);
+        faceRenderer.material.shader = DefaultShader;
+
+    }
+
+    void Update ()
+    {
+        //GetComponent<Renderer>().material = newFace;
+    }
+}
